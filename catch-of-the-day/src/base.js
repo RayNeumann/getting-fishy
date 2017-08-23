7603f96b203f849910268e31967ce283f8566278
Rami Buchbinder <ramiBuchbinder@users.noreply.github.com>
2017-08-23 16:17:25 +0200

    removing catch-of-the-day folder

--- catch-of-the-day/src/base.js
+++
@@ -1,9 +0,0 @@
-import Rebase from 're-base';
-
-const base = Rebase.createClass({
-    apiKey: "AIzaSyCnWck07HD3d3tpx1e8dGNKaeyCgB3nr88",
-    authDomain: "catch-of-the-day-d08ec.firebaseapp.com",
-    databaseURL: "https://catch-of-the-day-d08ec.firebaseio.com",
-});
-
-export default base;
